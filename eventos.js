aa0765607f541c5321053eeca609e9b2a8551bd6
vainntn <valentin.acosta512@gmail.com>
2023-08-07 13:51:40 -0300

    UpdateFinal

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const divBotonContainer = document.querySelector(".boton-container");
+    divBotonContainer.addEventListener("click", function () {
+        alert("Hola! Soy el div");
+    });
+});
